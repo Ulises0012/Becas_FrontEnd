dc4d887b5d891f1743f14861d79fe14e7569344c
Ulis3s0012 <ulisesandres0205@gmail.com>
2024-10-04 15:38:08 -0600

    Cambios realizados, se eliminan los controllers de la API y solo se crearan los controller para vistas, o sea esto pasara a ser el frontEnd

--- src/main/java/sv/org/arrupe/becas/config/WebConfig.java
+++ src/main/java/sv/org/arrupe/becas/config/WebConfig.java
@@ -2,6 +2,7 @@ package sv.org.arrupe.becas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // Aplicar el interceptor a las rutas que deseas proteger
-        registry.addInterceptor(authInterceptor()).addPathPatterns("/dashboard/**");
+        registry.addInterceptor(authInterceptor())
+                .addPathPatterns("/dashboard/**")  // Protege solo las rutas del dashboard
+                .excludePathPatterns("/login", "/api/**"); // Excluye el login y las rutas de API de autenticación
+    }
+
+    // Configuración CORS para permitir solicitudes entre el frontend y el backend
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica CORS a todas las rutas
+                .allowedOrigins("http://192.242.6.131", "http://localhost")  // Cambia por la IP o URL de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
